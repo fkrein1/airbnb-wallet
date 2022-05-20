e19ad1888e688bc1a8ebb15bc839b738bccd54d3
fkrein1 <felipekrein@gmail.com>
2022-05-19 21:34:22 -0300

    Updates

---
+++ precache-manifest.400075f5e075e2ef01e3115b7ff1e6dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52e568d7727de3c85f9bdf67161d0041",
+    "url": "/travel-wallet/index.html"
+  },
+  {
+    "revision": "5e9c8bed8b1f671dd3e8",
+    "url": "/travel-wallet/static/css/main.7ffaa5c9.chunk.css"
+  },
+  {
+    "revision": "3d7385e90497e9264f56",
+    "url": "/travel-wallet/static/js/2.b934acf5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/travel-wallet/static/js/2.b934acf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e9c8bed8b1f671dd3e8",
+    "url": "/travel-wallet/static/js/main.5387c293.chunk.js"
+  },
+  {
+    "revision": "f74bad08546b2e8c3ff7",
+    "url": "/travel-wallet/static/js/runtime-main.e94f38c9.js"
+  }
+]);
